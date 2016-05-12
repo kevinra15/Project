628153e26a71705262011c2b8409bf77c6cceb11
isx47752902 <isx47752902@i13.informatica.escoladeltreball.org>
2016-05-12 12:52:33 +0200

    Replace scripts to create conts

--- elasticsearch/image/create-cont-elastic.sh
+++ elasticsearch/image/create-cont-elastic.sh
@@ -1,47 +1,29 @@
 #!/bin/bash
-#Kevin Ramos Lopez
-#isx47752902
-#11/05/2016
+# Kevin Ramos Lopez
+# isx47752902
+# 11/05/2016
 
-########################################################################
-
-#Al executar aquest script es creara el contenidor docker de elasticsearch
+# En executar aquest script es creara la imatge i el contenidor
+# docker de elasticsearch
 
 #Instruccions i requisits
- #Executar en el directori on está el dockerfile
- #Tenir instalada una versió actual i funcional de docker
- #Disposar de connexió a internet
- #Permisos rwx per el directori /usr/share/elasticsearch/plugins/
+#	- Executar en el directori on está el Dockerfile
+#	- Tenir instalada una versió actual i funcional de docker
+#	- Disposar de connexió a Internet
  
 ########################################################################
 
+nameImage='image-elastic'
 nameContainer="cont-elastic"
 
-#Delete image with the same name
-
-
 #Create docker image
-docker build . > /dev/null
-
-#Check image
-docker images | grep "elasticsearch.*2.3  " > /dev/null
-if [ $? -ne 0 ]
-then
-	echo "Error: Image not created"
-	exit 1
-fi
+docker build --tag "$nameImage" --quiet  . 
 
 #Create container
-docker create --name $nameContainer elasticsearch:2.3 > /dev/null
+docker rm --force $nameContainer
+docker create --name $nameContainer "$nameImage"
 
-#Check container
-docker ps -a | grep "$nameContainer" > /dev/null
-if [ $? -ne 0 ]
-then
-	echo "Error: Container $nameContainer not created"
-	exit 2
-fi
+#Start container for development
+docker start --attach $nameContainer
 
-#Succesfully
-echo "Completed"
 exit 0
--- kibana/image/create-cont-kibana.sh
+++ kibana/image/create-cont-kibana.sh
@@ -3,18 +3,20 @@
 #isx47752902
 #11/05/2016
 
+########################################################################
 
-#Al executar aquest script es creara el contenidor docker de logstash
+#Al executar aquest script es creara el contenidor docker de elasticsearch
 
 #Instruccions i requisits
-#	Executar en el directori on está el dockerfile
-#	Tenir instalada una versió actual i funcional de docker
-#	Disposar de connexió a internet
+ #Executar en el directori on está el dockerfile
+ #Tenir instalada una versió actual i funcional de docker
+ #Disposar de connexió a internet
+ #Permisos rwx per el directori /usr/share/elasticsearch/plugins/
  
 ########################################################################
 
-nameImage='image-logstash'
-nameContainer="cont-logstash"
+nameContainer="cont-kibana"
+nameImage='image-kibana'
 
 #Create docker image
 docker build --tag "$nameImage" --quiet  . 
